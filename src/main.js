047f620083af8f039de1bf8880a5eb559ab5a5bd
Ruslan Khamitov <ruslan.khamitov@student.tuke.sk>
2024-12-20 19:03:06 +0100

    basic setup of main page

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import './css/style.css'
+
+document.querySelector('#app').innerHTML = `
+  <h1 style="font-weight: 500">[dev] <span style="font-weight: 800">г</span>uslan.</h1>
+  <div id="tupa-container">
+    <ul class="carousel">
+    <li><img class="list-item-img" src="/projects-images/dtit.png" alt="telekom hackathon project"></li>
+    <li><img class="list-item-img" src="/projects-images/smooth-abstract.jpg" alt="smooth abstract surface"></li>
+    <li><img class="list-item-img" src="/projects-images/erste.png" alt="erste hackathon project"></li>
+    <li><img class="list-item-img" src="/projects-images/pink-abstract.jpg" alt="pink abstract objects"></li>
+    <li><img class="list-item-img" src="/projects-images/reversi.png" alt="reversi fullstack project"></li>
+    </ul>
+  </div>
+`
